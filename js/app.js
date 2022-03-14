f054c4fd1885d85ec7ac4fad456893c98cf59362
ZD12T4NT <zacdistant@gmail.com>
2022-03-14 09:20:28 +0000

    commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav')
+
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('toggle')
+    nav.classList.toggle('nav-active')
+})
